5388c2694b1d7fe57bef831ffca21f4e3304b0d4
Bernardo Farrobinha <bernardofarrobinha1@gmail.com>
2023-02-09 17:14:45 +0000

    1

---
+++ api/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/helloWorld', function (req, res) {
+    res.status(200).send('Hello World');
+})
+
+app.get('/json', function (req, res) {
+    res.status(200).json({ message: 'Hello World' });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
