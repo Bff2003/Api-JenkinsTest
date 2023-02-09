78ea1e28786e0a6d2a15f01f5cb704bf02df538e
Bernardo Farrobinha <bernardofarrobinha1@gmail.com>
2023-02-09 16:32:08 +0000

    second commit

--- ApiTest.py
+++ ApiTest.py
@@ -7,11 +7,6 @@ link = "http://localhost:5000/api"
 
 class TestApi(unittest.TestCase):
 
-    def api_on(self):
-        response = requests.get(link + "/v1/hello")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.text, "Hello, World!")
-
     def test_hello(self):
         response = requests.get(link + "/v1/hello")
         self.assertEqual(response.status_code, 200)
